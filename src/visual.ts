436ea6aba0eb05a33f5caa5ebd4c3a5191dc990a
Pierrick Michel <pierrick_michel@csmv.qc.ca>
2018-01-09 16:10:40 -0500

    Suivi de tutoriel pour développement typescript et custom visual PBI

--- src/visual.ts
+++ src/visual.ts
@@ -1,58 +1,50 @@
-/*
- *  Power BI Visual CLI
- *
- *  Copyright (c) Microsoft Corporation
- *  All rights reserved.
- *  MIT License
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the ""Software""), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in
- *  all copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- *  THE SOFTWARE.
- */
 
 module powerbi.extensibility.visual {
-    "use strict";
+
+    //import printSymbol from '../assets/imprimante24x24.svg';
+
     export class Visual implements IVisual {
         private target: HTMLElement;
-        private updateCount: number;
         private settings: VisualSettings;
-        private textNode: Text;
+
+        private svgRoot: d3.Selection<SVGElementInstance>;
+        private ellipse: d3.Selection<SVGElementInstance>;
+        private image: d3.Selection<SVGElementInstance>;
+        private padding: number = 20;
+
 
         constructor(options: VisualConstructorOptions) {
-            console.log('Visual constructor', options);
+            /*
             this.target = options.element;
-            this.updateCount = 0;
             if (typeof document !== "undefined") {
                 const new_p: HTMLElement = document.createElement("p");
-                new_p.appendChild(document.createTextNode("Update count:"));
-                const new_em: HTMLElement = document.createElement("em");
-                this.textNode = document.createTextNode(this.updateCount.toString());
-                new_em.appendChild(this.textNode);
-                new_p.appendChild(new_em);
+                new_p.appendChild(document.createTextNode("Test CSMV"));
                 this.target.appendChild(new_p);
             }
+            */
+            
+            this.svgRoot = d3.select(options.element).append("svg");
+            this.ellipse = this.svgRoot.append("ellipse").style("fill", "rgba(255,255,0,0.5)").style("stroke", "rgba(0, 0, 0, 1.0)").style("stroke-width", "4");
+
+
         }
 
         public update(options: VisualUpdateOptions) {
+            console.log("Update Function");
+
             this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
-            console.log('Visual update', options);
-            if (typeof this.textNode !== "undefined") {
-                this.textNode.textContent = (this.updateCount++).toString();
-            }
+            
+            this.svgRoot.attr("width", options.viewport.width).attr("height", options.viewport.height);
+
+            var plot = {
+                xOffset: this.padding,
+                yOffset: this.padding,
+                width: options.viewport.width - (this.padding * 2),
+                height: options.viewport.height - (this.padding * 2)
+            };
+
+            this.ellipse.attr("cx", plot.xOffset + (plot.width * 0.5)).attr("cy", plot.yOffset + (plot.height * 0.5)).attr("rx", (plot.width * 0.5)).attr("ry", (plot.height * 0.5));
+
         }
 
         private static parseSettings(dataView: DataView): VisualSettings {
@@ -62,7 +54,6 @@ module powerbi.extensibility.visual {
         /** 
          * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the 
          * objects and properties you want to expose to the users in the property pane.
-         * 
          */
         public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
             return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
