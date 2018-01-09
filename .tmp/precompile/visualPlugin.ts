f0beb2945d38e87f46d8039cb6c71f70f5ee9dec
Pierrick Michel <pierrick_michel@csmv.qc.ca>
2018-01-09 11:57:58 -0500

    Création des éléments de base du custom visual PowerBI-PrintButton

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var powerBIPrintButton3925D33A881F43DB8F6511F7564627B2_DEBUG = {
+        name: 'powerBIPrintButton3925D33A881F43DB8F6511F7564627B2_DEBUG',
+        displayName: 'PowerBI PrintButton',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.10.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.powerBIPrintButton3925D33A881F43DB8F6511F7564627B2.Visual(options),
+        custom: true
+    };
+}
